6a131106efa29bc1d8cc91801795940e69efcba3
wangxuq <1228845897@qq.com>
2014-09-26 09:48:38 +0800

    fix error with index

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var Controllers = require('./controllers');
+//var Controllers = require('./controllers');
 
 var app = express();
 var port = process.env.PORT || 3000;
